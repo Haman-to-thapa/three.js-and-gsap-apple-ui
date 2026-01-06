12249df869ef4635e01d39a052b7c629049c616f
Haman-to-thapa <hamanthapa0000@gmail.com>
2026-01-06 16:42:26 +0530

    setup react

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+export const navLinks =  [
+              { label: 'Store' },
+              { label: 'Mac' },
+              { label: 'Linux' },
+              { label: 'iPhone' },
+              { label: 'Watch' },
+              { label: 'Vision' },
+              { label: 'AirPods' }
+            ]
